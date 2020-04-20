1b71b36e25a533364383e9d3433e28083187f3f2
Vince <vincentbollaert@gmail.com>
2020-04-20 14:33:35 +0700

    add remove

--- src/pages/Home/Todos/component.jsx
+++ src/pages/Home/Todos/component.jsx
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 import { useSelector, useDispatch } from 'react-redux'
 import { actions } from '../../../state/todos/reducer'
 import { SIZE_XLG, SIZE_SM, SIZE_LG } from '../../../styles'
+import binSvg from '../../../assets/svg/bin.svg'
+import Svg from '../../../components/Svg/component'
 
 const Wrap = styled.div`
   width: 240px;
@@ -16,6 +18,8 @@ const Title = styled.div`
   margin-bottom: ${SIZE_LG};
 `
 const Todo = styled.div`
+  position: relative;
+  display: flex;
   margin-bottom: ${SIZE_SM};
 
   &:last-child {
@@ -23,11 +27,32 @@ const Todo = styled.div`
   };
 `
 const Name = styled.div`
+  padding-right: 1.6rem;
   cursor: pointer;
 
   ${p => p.isDone && `
     opacity: 0.6;
   `};
+`
+const Actions = styled.div`
+  display: none;
+  position: absolute;
+  right: 0;
+
+  ${Todo}:hover & {
+    display: flex;
+  };
+`
+const Remove = styled(Svg)`
+  width: 1.6rem;
+  height: 1.6rem;
+  cursor: pointer;
+  opacity: 0.4;
+
+  &:hover {
+    fill: red;
+    opacity: 1;
+  };
 `
 
 const Todos = () => {
@@ -44,6 +69,9 @@ const Todos = () => {
           <Name isDone={isDone} onClick={() => dispatch(actions.toggleIsDone(id))}>
             {todoName}
           </Name>
+          <Actions>
+            <Remove svg={binSvg} onClick={() => dispatch(actions.remove(id))} />
+          </Actions>
         </Todo>
       ))}
     </Wrap>
