1e12a7b836086be401e693342b7942e2839f9382
Vince <vincentbollaert@gmail.com>
2020-04-20 18:31:58 +0700

    removed todos submit button

--- src/pages/Home/AddNewTodo/component.jsx
+++ src/pages/Home/AddNewTodo/component.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import { useForm } from 'react-hook-form'
 import styled from 'styled-components'
 import TextField from '../../../components/form/Field/component'
+import { SIZE_LG } from '../../../styles'
 
+const Form = styled.form`
+  margin-bottom: ${SIZE_LG};
+`
 function AddNewTodo({ addNewTodo }) {
   const { register, handleSubmit, errors } = useForm()
   const onSubmit = data => addNewTodo(data)
   const errorMessage = (errors.todo || {}).type
   
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <Form onSubmit={handleSubmit(onSubmit)}>
       <TextField
         theme='light'
         name="todo"
@@ -17,9 +21,7 @@ function AddNewTodo({ addNewTodo }) {
         errorMessage={errorMessage}
         inputRef={register({ required: true, maxLength: 80 })}
       />
-
-      <input type="submit" />
-    </form>
+    </Form>
   )
 }
 
