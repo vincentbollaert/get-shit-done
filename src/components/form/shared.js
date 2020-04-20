162cf1ad15257fc1563456995137fb807e8947a0
Vince <vincentbollaert@gmail.com>
2020-04-20 18:23:23 +0700

    added form shared styles file

---
+++ src/components/form/shared.js
@@ -0,0 +1,16 @@
+import { SONIC_SILVER, SIZE_MD, SIZE_XSM, ISABELLINE, SUNSET_ORANGE } from '../../styles'
+
+export const STYLE_COLOR_LIGHT = ISABELLINE
+export const STYLE_COLOR_DARK = 'red'
+
+export const STYLE_BORDER_COLOR = 'rgba(213, 213, 213, 0.62)'
+
+export const STYLE_UNDERLINE = '0 2px 0 -1px #e5e5e5'
+export const STYLE_UNDERLINE_HAS_VALUE = `0 2px 0 -1px ${SONIC_SILVER}`
+export const STYLE_UNDERLINE_ERROR = `0 2px 0 -1px ${SUNSET_ORANGE}`
+
+export const STYLE_PLACEHOLDER_COLOR_DARK = 'rgba(0, 0, 0, 0.3)'
+export const STYLE_PLACEHOLDER_COLOR_LIGHT = 'rgba(255, 255, 255, 0.6)'
+
+export const STYLE_HAS_VALUE_PADDING = `${SIZE_MD} 0 ${SIZE_XSM};`
+export const STYLE_HEIGHT = '3.2rem'
