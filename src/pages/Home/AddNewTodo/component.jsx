4050e39677469878fd3f2ffb093215fea886b1c6
Vince <vincentbollaert@gmail.com>
2020-04-20 18:24:58 +0700

    implemented field in todo compnent

--- src/pages/Home/AddNewTodo/component.jsx
+++ src/pages/Home/AddNewTodo/component.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
+import styled from 'styled-components'
+import TextField from '../../../components/form/Field/component'
 
 function AddNewTodo({ addNewTodo }) {
   const { register, handleSubmit, errors } = useForm()
   const onSubmit = data => addNewTodo(data)
-  console.log(errors)
+  const errorMessage = (errors.todo || {}).type
   
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="text" placeholder="Add todo" name="todo" ref={register({required: true, maxLength: 80})} />
+      <TextField
+        theme='light'
+        name="todo"
+        placeholder="add todo"
+        errorMessage={errorMessage}
+        inputRef={register({ required: true, maxLength: 80 })}
+      />
 
       <input type="submit" />
     </form>
