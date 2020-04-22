f62a2d9abeabd323bca258c736d6e8742ddea15f
Vince <vincentbollaert@gmail.com>
2020-04-22 17:02:16 +0700

    removed compression lib, set headers to br if supported, other gz

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const app = express()
-const compression = require('compression')
 
-app.use(compression())
+app.get('*.js', (req, res, next) => {
+  if (req.header('Accept-Encoding').includes('br')) {
+    req.url = req.url + '.br';
+    res.set('Content-Encoding', 'br');
+    res.set('Content-Type', 'application/javascript; charset=UTF-8');
+  } else {
+    req.url = req.url + '.gz';
+    res.set('Content-Encoding', 'gzip');
+    res.set('Content-Type', 'application/javascript; charset=UTF-8');
+  }
+  next();
+});
+
 app.use(express.static('../client/public'))
 
 const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log('Listening on port ' + listener.address().port);
 });
