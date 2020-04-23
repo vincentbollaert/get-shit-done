8fb297473cc747e7e38cb5338fa23a8deab8420d
Vince <vincentbollaert@gmail.com>
2020-04-23 19:14:09 +0700

    modified useFilterHours to work with new filtering mech

--- client/src/hooks/useFilterHours.js
+++ client/src/hooks/useFilterHours.js
@@ -2,18 +2,10 @@ import { useState } from 'react'
 
 const useFilterHours = () => {
   const hoursInDay = Array(24).fill(null).map((item, index) => index)
-  const [min, setMin] = useState(0)
-  const [max, setMax] = useState(23)
   const [hoursToDisplay, setHoursToDisplay] = useState(hoursInDay)
 
-  const filterHours = (event) => {
-    const { name, value } = event.target
-    if (name === 'min') {
-      setMin(value)
-    } else {
-      setMax(value)
-    }
-    setHoursToDisplay(hoursInDay.filter(hour => hour >= min && hour <= max))
+  const filterHours = ({ from, to }) => {
+    setHoursToDisplay(hoursInDay.filter(hour => hour >= from && hour <= to))
   }
 
   return [hoursToDisplay, filterHours]
