094b237ca5af79602a4591a17c3ce75c9de2200e
Vince <vincentbollaert@gmail.com>
2020-04-23 14:27:23 +0700

    update today's time

--- client/src/pages/Home/CurrentTime/component.jsx
+++ client/src/pages/Home/CurrentTime/component.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import styled from 'styled-components'
 import differenceInMinutes from 'date-fns/differenceInMinutes'
 import startOfToday from 'date-fns/startOfToday'
 import format from 'date-fns/format'
+import add from 'date-fns/add'
 
 const Wrap = styled.div`
   position: absolute;
@@ -22,13 +23,25 @@ const Time = styled.span`
 `
 const MINUTES_IN_DAY = 1440
 
-const CurrentTime = ({ date }) => {
-  const nowInMinutes = differenceInMinutes(new Date(), startOfToday())
-  console.log(nowInMinutes / MINUTES_IN_DAY * 100)
+const CurrentTime = () => {
+  const [currentTime, setCurrentTime] = useState(new Date())
+  const currentTimeRef = useRef(null)
+  const date = new Date()
+  const nowInMinutes = differenceInMinutes(date, startOfToday())
+  currentTimeRef.current = currentTime
+
+  function updateTime() {
+    setCurrentTime(add(currentTimeRef.current, { seconds: 1 }))
+  }
+
+  useEffect(() => {
+    window.setInterval(updateTime, 1000)
+  }, [])
+
   return (
     <Wrap top={nowInMinutes / MINUTES_IN_DAY * 100}>
       <Time>
-        {format(date, 'p')}
+        {format(currentTime, 'p')}
       </Time>
     </Wrap>
   )
