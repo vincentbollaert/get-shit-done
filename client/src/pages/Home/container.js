482afbf6d37c09a46453d1815ba287826033fa64
Vince <vincentbollaert@gmail.com>
2020-04-25 15:54:11 +0700

    removed unused container component

--- client/src/pages/Home/container.js
+++
@@ -1,8 +0,0 @@
-import { connect } from 'react-redux'
-import Component from './component'
-
-const mapStateToProps = ({ todos }) => ({
-  todos,
-})
-
-export default connect(mapStateToProps)(Component)
