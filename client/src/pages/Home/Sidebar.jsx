28f21b9c4bb608eb9ee80758a419860fdf90f4c9
Vince <vincentbollaert@gmail.com>
2020-04-28 14:38:52 +0700

    updated design of sidebar
    and put actions to right to keep them positioned consistently

--- client/src/pages/Home/Sidebar.jsx
+++ client/src/pages/Home/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
-import { SONIC_SILVER, PASTEL_GRAY, ONYX, JET, SIZE_XLG, ISABELLINE, QUICK_SILVER } from '../../styles'
+import { SONIC_SILVER, PASTEL_GRAY, ONYX, JET, SIZE_XLG, ISABELLINE, QUICK_SILVER, STYLE_TRANSITION, CHARCOAL } from '../../styles'
 import lisSvg from '../../assets/svg/list.svg'
 import Svg from '../../components/Svg/component'
 
@@ -9,17 +9,13 @@ const Wrap = styled.div`
   position: relative;
   display: flex;
   padding-right: 40px;
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
 `
 const Tabs = styled.div`
   z-index: 1;
   position: absolute;
   right: 0;
   height: 100%;
-  background-color: #3d4150;
+  background-color: ${CHARCOAL};
   width: 40px;
   align-items: center;
   justify-content: center;
@@ -57,24 +53,26 @@ const SvgStyled = styled(Svg)`
 `
 
 const Content = styled.div`
-  position: relative;
+  position: absolute;
+  top: 0;
+  right: 40px;
+  bottom: 0;
   flex-direction: column;
   flex-grow: 1;
   padding: ${SIZE_XLG};
   width: 240px;
   color: ${ISABELLINE};
-  background-color: #3d4150;
+  background-color: ${CHARCOAL};
   box-shadow: inset -1px 0 0 0px #4f5466;
   transform: translateX(100%);
-  transition: transform 0.1s ease-out;
+  transition: transform ${STYLE_TRANSITION};
 
   ${p => p.isOpen && `
     transform: translateX(0);
   `};
 `
 
-const Sidebar = ({ isRight, children }) => {
-  const [isOpen, setIsOpen] = useState(false)
+const Sidebar = ({ isOpen, setIsOpen, children }) => {
 
   return (
     <Wrap>
