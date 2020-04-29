206a49561e4e23f97c289c248591f053bb3bbb2b
Vince <vincentbollaert@gmail.com>
2020-04-29 11:38:30 +0700

    added more colors, randomly assign tasks and converted all to rgb

--- client/src/reducers/calendar.js
+++ client/src/reducers/calendar.js
@@ -1,7 +1,23 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit'
 import format from 'date-fns/format'
 import { MONTH_DAYS, MONTH_DAYS_STRING, HOURS_IN_DAY } from '../constants'
+import { dummyColors } from './settings'
 
+const generateRandomIndex = () => Math.floor(Math.random() * (15 - 0 + 1)) + 0
+
+const colors = {
+  dummy1: dummyColors[generateRandomIndex()],
+  dummy2: dummyColors[generateRandomIndex()],
+  dummy3: dummyColors[generateRandomIndex()],
+  dummy4: dummyColors[generateRandomIndex()],
+  dummy5: dummyColors[generateRandomIndex()],
+  dummy6: dummyColors[generateRandomIndex()],
+  dummy7: dummyColors[generateRandomIndex()],
+  dummy8: dummyColors[generateRandomIndex()],
+  dummy9: dummyColors[generateRandomIndex()],
+  dummy10: dummyColors[generateRandomIndex()],
+  dummy11: dummyColors[generateRandomIndex()],
+}
 const initialState = {
   allTasksByDay: MONTH_DAYS_STRING.map((dateString) => ({
     tasks: [
@@ -9,67 +25,67 @@ const initialState = {
         id: nanoid(),
         time: [0, 6.75],
         name: 'sleep',
-        color: 'bone',
+        color: colors.dummy1,
       },
       {
         id: nanoid(),
         time: [6.75, 7.25],
         name: 'morning routine someting somethignsdsdssdsdsd',
-        color: 'laurel_green',
+        color: colors.dummy2,
       },
       {
         id: nanoid(),
         time: [7.25, 8],
         name: 'something after morning',
-        color: 'dark_sea_green',
+        color: colors.dummy3,
       },
       {
         id: nanoid(),
         time: [8, 12],
         name: 'another thing',
-        color: 'bone',
+        color: colors.dummy4,
       },
       {
         id: nanoid(),
         time: [12, 17],
         name: 'work',
-        color: 'dark_sea_green',
+        color: colors.dummy5,
       },
       {
         id: nanoid(),
         time: [17, 17.5],
         name: 'work',
-        color: 'dark_sea_green',
+        color: colors.dummy6,
       },
       {
         id: nanoid(),
         time: [18, 19],
         name: 'work',
-        color: 'dark_sea_green',
+        color: colors.dummy7,
       },
       {
         id: nanoid(),
         time: [19, 21],
         name: 'work',
-        color: 'dark_sea_green',
+        color: colors.dummy8,
       },
       {
         id: nanoid(),
         time: [21, 21.25],
         name: 'work',
-        color: 'dark_sea_green',
+        color: colors.dummy9,
       },
       {
         id: nanoid(),
         time: [21.25, 22],
         name: 'work',
-        color: 'dark_sea_green',
+        color: colors.dummy10,
       },
       {
         id: nanoid(),
         time: [22, 24],
         name: 'work',
-        color: 'dark_sea_green',
+        color: colors.dummy11,
       },
     ],
     dateString,
