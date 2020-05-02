6b68bffc0d83aea6fbb9b0cbd3a4f28b8ee4733d
Vince <vincentbollaert@gmail.com>
2020-05-02 18:55:41 +0700

    extracted style_ellipsis

--- client/src/styles/index.js
+++ client/src/styles/index.js
@@ -3,3 +3,10 @@ export { default as reset } from './reset'
 
 export const STYLE_TRANSITION = '0.2s ease-out'
 export const STYLE_SIDEBAR_WIDTH_UNIT = '24'
+
+
+export const STYLE_ELLIPSIS = `
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`
