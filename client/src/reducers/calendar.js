d5e7b17a37e3c83950e3cab06c1671925faf6733
Vince <vincentbollaert@gmail.com>
2020-05-13 16:11:46 +0700

    update calendar dummy data

--- client/src/reducers/calendar.js
+++ client/src/reducers/calendar.js
@@ -273,8 +273,26 @@ const initialState = {
         },
         {
           id: nanoid(),
-          time: [14, 20],
+          time: [14, 16],
           name: 'dev: get shit done',
+          group: 'improvement',
+        },
+        {
+          id: nanoid(),
+          time: [16, 18],
+          name: 'shopping with mai',
+          group: 'essentials',
+        },
+        {
+          id: nanoid(),
+          time: [18, 19],
+          name: 'relax',
+          group: 'laze',
+        },
+        {
+          id: nanoid(),
+          time: [19, 20],
+          name: 'watch work',
           group: 'improvement',
         },
         {
@@ -282,6 +300,362 @@ const initialState = {
           time: [20, 22.5],
           name: 'relax',
           group: 'laze',
+        },
+      ]
+    }
+    if (index === 5) {
+      tasks = [
+        {
+          id: nanoid(),
+          time: [0, 6.5],
+          name: 'sleep',
+          group: 'essentials',
+        },
+        {
+          id: nanoid(),
+          time: [6.5, 7],
+          name: 'morning routine',
+          group: 'essentials',
+        },
+        {
+          id: nanoid(),
+          time: [7, 10.75],
+          name: 'watch/do work',
+          group: 'improvement',
+        },
+        {
+          id: nanoid(),
+          time: [10.75, 14],
+          name: 'lunch/find new phone',
+          group: 'essentials',
+        },
+        {
+          id: nanoid(),
+          time: [14, 16],
+          name: 'watch/do work',
+          group: 'improvement',
+        },
+        {
+          id: nanoid(),
+          time: [16, 17.5],
+          name: 'dishes, house cleaning',
+          group: 'essentials',
+        },
+        {
+          id: nanoid(),
+          time: [17.5, 18],
+          name: 'relax',
+          group: 'laze',
+        },
+        {
+          id: nanoid(),
+          time: [18, 19],
+          name: 'watch/do work',
+          group: 'improvement',
+        },
+        {
+          id: nanoid(),
+          time: [19, 19.5],
+          name: 'supper',
+          group: 'essentials',
+        },
+        {
+          id: nanoid(),
+          time: [19.5, 21],
+          name: 'watch/do work',
+          group: 'improvement',
+        },
+        {
+          id: nanoid(),
+          time: [21, 23.5],
+          name: 'relax',
+          group: 'laze',
+        },
+      ]
+    }
+    if (index === 6) {
+      tasks = [
+        {
+          id: nanoid(),
+          time: [0, 8],
+          name: 'sleep',
+          group: 'essentials',
+        },
+        {
+          id: nanoid(),
+          time: [8, 8.5],
+          name: 'watch upskill in bed',
+          group: 'improvement',
+        },
+        {
+          id: nanoid(),
+          time: [8.5, 11.5],
+          name: 'morning routine/chores',
+          group: 'essentials',
+        },
+        {
+          id: nanoid(),
+          time: [11.5, 14.5],
+          name: 'watch/do work',
+          group: 'improvement',
+        },
+        {
+          id: nanoid(),
+          time: [14.5, 15],
+          name: 'shopping',
+          group: 'essentials',
+        },
+        {
+          id: nanoid(),
+          time: [15, 22.5],
+          name: 'play with new phone/relax',
+          group: 'laze',
+        },
+      ]
+    }
+    if (index === 7) {
+      tasks = [
+        {
+          id: nanoid(),
+          time: [0, 8],
+          name: 'sleep',
+          group: 'essentials',
+        },
+        {
+          id: nanoid(),
+          time: [8, 9],
+          name: 'play with phone in bed',
+          group: 'laze',
+        },
+        {
+          id: nanoid(),
+          time: [9, 9.5],
+          name: 'morning routine/chores',
+          group: 'essentials',
+        },
+        {
+          id: nanoid(),
+          time: [9.5, 10.5],
+          name: 'watch youtube',
+          group: 'laze',
+        },
+        {
+          id: nanoid(),
+          time: [10.5, 14],
+          name: 'watch/do work',
+          group: 'improvement',
+        },
+        {
+          id: nanoid(),
+          time: [14, 14.5],
+          name: 'relax',
+          group: 'laze',
+        },
+        {
+          id: nanoid(),
+          time: [14.5, 15.75],
+          name: 'watch/do work',
+          group: 'improvement',
+        },
+        {
+          id: nanoid(),
+          time: [15.75, 16],
+          name: 'relax',
+          group: 'laze',
+        },
+        {
+          id: nanoid(),
+          time: [16, 18.5],
+          name: 'watch/do work',
+          group: 'improvement',
+        },
+        {
+          id: nanoid(),
+          time: [18.5, 19],
+          name: 'relax',
+          group: 'laze',
+        },
+        {
+          id: nanoid(),
+          time: [19, 19.5],
+          name: 'dev: boilerplate',
+          group: 'improvement',
+        },
+        {
+          id: nanoid(),
+          time: [19.5, 23],
+          name: 'relax',
+          group: 'laze',
+        },
+      ]
+    }
+    if (index === 8) {
+      tasks = [
+        {
+          id: nanoid(),
+          time: [0, 6],
+          name: 'sleep',
+          group: 'essentials',
+        },
+        {
+          id: nanoid(),
+          time: [6, 7.5],
+          name: 'youtube in bed',
+          group: 'laze',
+        },
+        {
+          id: nanoid(),
+          time: [7.5, 8.5],
+          name: 'morning routine',
+          group: 'essentials',
+        },
+        {
+          id: nanoid(),
+          time: [8.5, 11.5],
+          name: 'work/watch',
+          group: 'improvement',
+        },
+        {
+          id: nanoid(),
+          time: [11.5, 14.5],
+          name: 'relax',
+          group: 'laze',
+        },
+        {
+          id: nanoid(),
+          time: [14.5, 18],
+          name: 'work/watch',
+          group: 'improvement',
+        },
+      ]
+    }
+    if (index === 9) {
+      tasks = [
+        {
+          id: nanoid(),
+          time: [0, 7.5],
+          name: 'sleep',
+          group: 'essentials',
+        },
+        {
+          id: nanoid(),
+          time: [7.5, 17.25],
+          name: 'relax',
+          group: 'laze',
+        },
+        {
+          id: nanoid(),
+          time: [17.25, 20],
+          name: 'work/watch',
+          group: 'improvement',
+        },
+      ]
+    }
+    if (index === 10) {
+      tasks = [
+        {
+          id: nanoid(),
+          time: [0, 7.5],
+          name: 'sleep',
+          group: 'essentials',
+        },
+        {
+          id: nanoid(),
+          time: [7.5, 9.5],
+          name: 'morning-routine/relax',
+          group: 'laze',
+        },
+        {
+          id: nanoid(),
+          time: [9.5, 17.5],
+          name: 'work/watch',
+          group: 'improvement',
+        },
+        {
+          id: nanoid(),
+          time: [17.5, 18],
+          name: 'interview',
+          group: 'work',
+        },
+        {
+          id: nanoid(),
+          time: [18, 19.5],
+          name: 'relax',
+          group: 'laze',
+        },
+        {
+          id: nanoid(),
+          time: [19.5, 20.5],
+          name: 'interview',
+          group: 'work',
+        },
+        {
+          id: nanoid(),
+          time: [20, 23],
+          name: 'relax',
+          group: 'laze',
+        },
+      ]
+    }
+    if (index === 11) {
+      tasks = [
+        {
+          id: nanoid(),
+          time: [0, 8],
+          name: 'sleep',
+          group: 'essentials',
+        },
+        {
+          id: nanoid(),
+          time: [8, 9],
+          name: 'lie in bed',
+          group: 'laze',
+        },
+        {
+          id: nanoid(),
+          time: [9, 23],
+          name: 'relax. off day',
+          group: 'laze',
+        },
+      ]
+    }
+    if (index === 12) {
+      tasks = [
+        {
+          id: nanoid(),
+          time: [0, 6.5],
+          name: 'sleep',
+          group: 'essentials',
+        },
+        {
+          id: nanoid(),
+          time: [6.5, 9],
+          name: 'relax in bed',
+          group: 'laze',
+        },
+        {
+          id: nanoid(),
+          time: [9, 12],
+          name: 'clean flat',
+          group: 'essentials',
+        },
+        {
+          id: nanoid(),
+          time: [12, 13],
+          name: 'play pool',
+          group: 'leasure',
+        },
+        {
+          id: nanoid(),
+          time: [13, 16],
+          name: 'prepare post-interview options',
+          group: 'planning',
+        },
+        {
+          id: nanoid(),
+          time: [16, 17],
+          name: 'dev: get shit done',
+          group: 'improvement',
         },
       ]
     }
