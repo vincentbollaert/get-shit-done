b05d57342a56c050871204911a5b00660c58c1fb
Vince <vincentbollaert@gmail.com>
2020-05-18 14:46:19 +0700

    move custom scrollbars to css utils

--- client/src/styles/utils.js
+++ client/src/styles/utils.js
@@ -1,3 +1,4 @@
+// https://gist.github.com/vincentbollaert/e90def9b351d8d97c90ef7cfd887685e
 // https://jsperf.com/color-darken
 
 export function rgbAdjust(color, amount) {
@@ -27,6 +28,35 @@ export const ellipsis = () => `
   text-overflow: ellipsis;
 `
 
+export const scrollbarMinLight = () => `
+  ::-webkit-scrollbar {
+    width: 4px;
+    height: 4px;
+    background-color: transparent;
+    border-left: 1px solid var(--white-smoke);
+  }
+
+  ::-webkit-scrollbar-track {
+    display: none;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    border: none;
+    border-radius: 0;
+    background: #b3c1c5;
+  }
+
+  ::-webkit-scrollbar-button {
+    display: none;
+  }
+
+  ::-webkit-scrollbar-corner {
+    background-color: transparent;
+  }
+`
+
+
+// media query
 const customMediaQuery = minWidth => `@media (min-width: ${minWidth}px)`
 
 export const media = {
